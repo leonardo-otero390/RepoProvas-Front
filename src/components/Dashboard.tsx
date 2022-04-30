d8fffef3171c9a69084ba487a1947932e567af9e
Leonardo Otero <leonardo.otero390@gmail.com>
2022-04-29 20:25:05 -0400

    feat: debounce input

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,25 +1,33 @@
-import { useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import Header from "../components/Header";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
+import SearchInput from "./SearchInput";
+import { useState } from "react";
+import { Box } from "@mui/material";
 
 interface Props {
   type: "disciplines" | "teachers";
 }
 
+const styles = {
+  container: { width: "80%", maxWidth: 700, margin: "16px auto" },
+  hr: { color: "#C4C4C4" },
+};
+
 export default function Dashboard({ type }: Props) {
+  const [name, setName] = useState("");
   if (type !== "disciplines" && type !== "teachers") type = "disciplines";
   const navigate = useNavigate();
-  const title =
+  const placeholder =
     type === "disciplines" ? "Buscar por disciplina" : "Buscar por professores";
-
   return (
     <>
       <Header />
-      <Title>{title}</Title>
-      <hr color="#C4C4C4" />
-      <Container>
+      <SearchInput placeholder={placeholder} setName={setName} />
+      <hr style={styles.hr} />
+      <Box sx={styles.container}>
         <Stack spacing={16} direction="row">
           <Button
             variant={type === "disciplines" ? "contained" : "outlined"}
@@ -35,24 +43,7 @@ export default function Dashboard({ type }: Props) {
           </Button>
           <Button variant="outlined">Adicionar</Button>
         </Stack>
-      </Container>
+      </Box>
     </>
   );
 }
-
-const Title = styled.h1`
-  width: 100%;
-  text-align: center;
-  font-family: "Poppins";
-  font-size: 24px;
-  line-height: 24px;
-  letter-spacing: 0.15px;
-  color: rgba(0, 0, 0, 0.8);
-  margin: 16px auto 24px auto;
-`;
-
-const Container = styled.main`
-  width: 80%;
-  max-width: 700px;
-  margin: 16px auto;
-`;
---
+++ src/components/SearchInput.tsx
@@ -0,0 +1,45 @@
+import { Box, TextField } from "@mui/material";
+import { useState } from "react";
+
+interface Props {
+  setName: React.Dispatch<React.SetStateAction<string>>;
+  placeholder: string;
+}
+
+const styles = {
+  container: {
+    width: "100%",
+    display: "flex",
+  },
+  input: {
+    margin: "8px auto",
+  },
+};
+
+export default function SearchInput({ setName, placeholder }: Props) {
+  const [value, setValue] = useState("");
+
+  let timer: NodeJS.Timeout | null = null;
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setValue(event.target.value);
+
+    const debounceTimeout = 300;
+
+    if (value.length < 2) return;
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(() => {
+      setName(event.target.value);
+    }, debounceTimeout);
+  }
+  return (
+    <Box sx={styles.container}>
+      <TextField
+        placeholder={placeholder}
+        value={value}
+        sx={styles.input}
+        onChange={handleChange}
+      />
+    </Box>
+  );
+}
